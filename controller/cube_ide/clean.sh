67342374804f170b5ebb5a7a291a67ebe395cbbe
Zapta <zapta@users.noreply.github.com>
2023-05-16 23:00:02 -0700

    Added a skeleton controller kicad project. Need to be renamed.

---
+++ controller/cube_ide/clean.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# per https://stackoverflow.com/questions/46273032
+
+# Delete ignored files
+git clean -dfX .
+
+
---
+++ controller/cube_ide/copy_core.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -ex
+
+rm -r ../platformio/lib/Core/
+cp -r Core/ ../platformio/lib/Core/
+
