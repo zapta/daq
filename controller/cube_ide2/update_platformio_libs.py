96b44651b032e628ab4b23817928e6f4eb2f6437
Zapta <zapta@users.noreply.github.com>
2023-05-29 09:17:31 -0700

    Updated the cube_ide lib copying script.

--- controller/cube_ide2/update_platformio_libs.py
+++ controller/cube_ide2/update_platformio_libs.py
@@ -13,7 +13,9 @@ def force_empty_dir(path):
   assert os.path.exists(path)
 
 
-def create_flatten_directory(src, dst):
+def create_flatten_directory(src, dst, suppressed_list = []):
+  suppressed_set = set(suppressed_list)
+  assert len(suppressed_set) == len(suppressed_set)
   force_empty_dir(dst)
   # List files recursively.
   file_list = []
@@ -21,14 +23,21 @@ def create_flatten_directory(src, dst):
     for file in files:
       path = os.path.join(root, file);
       file_list.append(path.replace("\\", "/"))
-  # Copy files      
+  # Copy files     
+  suppressed_found = set()
   for src_file in file_list:
     basename = os.path.basename(src_file)
     dst_file = os.path.join(dst, basename).replace("\\", "/")
+    if basename in suppressed_set:
+      assert not basename in suppressed_found
+      suppressed_found.add(basename)
+      dst_file += ".ignored"
     print(f"Copying {src_file} -> {dst_file}")
     assert not os.path.exists(dst_file)
     shutil.copyfile(src_file, dst_file)
     assert os.path.exists(dst_file)
+  # Make sure we identified all the suppressed.
+  assert suppressed_found == suppressed_set
 
 
 DST="../platformio2/lib"
@@ -36,7 +45,7 @@ assert os.path.exists(DST)
 
 
 force_empty_dir("flat_lib")
-create_flatten_directory("Core", f"{DST}/autogen_core")
+create_flatten_directory("Core", f"{DST}/autogen_core", {"main.c", "freertos.c"})
 create_flatten_directory("USB_DEVICE", f"{DST}/autogen_usb")
 create_flatten_directory("Middlewares", f"{DST}/autogen_middlewares")
 
