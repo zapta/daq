5bcd9aa45e7484ba17c6dfc18f4cb1410329beed
Zapta <zapta@users.noreply.github.com>
2023-06-02 14:02:36 -0700

    Enabled support for hardware floating point.

---
+++ controller/platformio/extra_script.py
@@ -0,0 +1,24 @@
+# Per https://docs.platformio.org/en/latest/scripting/examples/extra_linker_flags.html
+
+Import("env")
+
+#
+# Dump build environment (for debug)
+# print(env.Dump())
+#
+
+# To support hardware FP also by the linker, we have to 
+# pass -mfloat-abi=hard here in addition to platformio.ini 
+env.Append(
+  LINKFLAGS=[
+    "-mfpu=fpv5-d16",
+    "-mfloat-abi=hard",
+    "-mthumb"
+  ]
+)
+
+# -mcpu=cortex-m7 
+# -T"C:\Users\user\projects\daq\repo\controller\cube_ide\STM32H750VBTX_FLASH.ld" 
+# --specs=nosys.specs -Wl,-Map="cube_ide.map" -Wl,--gc-sections -static 
+# --specs=nano.specs -mfpu=fpv5-d16 -mfloat-abi=hard -mthumb -Wl,--start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group
+# Finished building target: cube_ide.elf
