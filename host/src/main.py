952f14c717de6fccb6b93bce9101d534b4efc58e
Zapta <zapta@users.noreply.github.com>
2023-06-18 08:48:10 -0700

    Added a flag for host program output file.

--- host/src/main.py
+++ host/src/main.py
@@ -6,10 +6,12 @@
 from __future__ import annotations
 
 import sys
+import os
+import atexit
 
 # For using the local version of serial_packet. Comment out if
 # using serial_packets package installed by pip.
-sys.path.insert(0, "../../../../serial_packets_py/repo/src")
+# sys.path.insert(0, "../../../../serial_packets_py/repo/src")
 
 import argparse
 import asyncio
@@ -26,20 +28,31 @@ logger = logging.getLogger("main")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--port", dest="port", default=None, help="Serial port to use.")
+parser.add_argument("--output_file", dest="output_file", default="data_output_file.txt", help="Name of output file.")
 args = parser.parse_args()
 
+output_file = None
 
-async def command_async_callback(
-    endpoint: int, data: PacketData
-) -> Tuple[int, PacketData]:
+
+def atexit_handler():
+    global output_file
+    if output_file: 
+        logger.info(f"Flushing data file [{args.output_file}]")
+        output_file.flush()
+        output_file = None
+    os._exit(0)
+
+
+atexit.register(atexit_handler)
+
+
+async def command_async_callback(endpoint: int, data: PacketData) -> Tuple[int, PacketData]:
     logger.info(f"Received command: [%d] %s", endpoint, data.hex_str(max_bytes=10))
     # In this example we don't expect incoming commands at the master side.
     return (PacketStatus.UNHANDLED.value, PacketData())
 
 
-async def message_async_callback(
-    endpoint: int, data: PacketData
-) -> Tuple[int, PacketData]:
+async def message_async_callback(endpoint: int, data: PacketData) -> Tuple[int, PacketData]:
     logger.info(f"Received message: [%d] %s", endpoint, data.hex_str(max_bytes=10))
     if endpoint == 10:
         handle_adc_report_message(data)
@@ -54,22 +67,25 @@ async def event_async_callback(event: PacketsEvent) -> None:
 
 
 def handle_adc_report_message(data: PacketData):
+    global output_file
     data.reset_read_location()
-    with open("test.txt", "a") as myfile:
-      while not data.all_read():
-          val = data.read_int24()
-          if data.read_error():
+    if not output_file:
+        logger.info(f"Opening output data file [{args.output_file}]")
+        output_file = open(args.output_file, "w")
+    while not data.all_read():
+        val = data.read_int24()
+        if data.read_error():
             break
-          myfile.write(f"{val}\n")
+        output_file.write(f"{val}\n")
     if data.read_error():
-      print(f"ERROR: Error while parsing an adc report message.", flush=True)
+        logger.error("Error while parsing an adc report message.")
+
 
 async def async_main():
     logger.info("Started.")
     assert args.port is not None
-    client = SerialPacketsClient(
-        args.port, command_async_callback, message_async_callback, event_async_callback
-    )
+    client = SerialPacketsClient(args.port, command_async_callback, message_async_callback,
+                                 event_async_callback)
     while True:
         # Connect if needed.
         if not client.is_connected():
