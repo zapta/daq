d4889b8f6d81ec69c135f46dce1c2f68ee666557
Zapta <zapta@users.noreply.github.com>
2023-08-19 12:27:51 -0700

    Fixed unity issues regarding test_port and automatic dependency detection.

--- controller/platformio/extra_script.py
+++ controller/platformio/extra_script.py
@@ -1,5 +1,7 @@
 # Per https://docs.platformio.org/en/latest/scripting/examples/extra_linker_flags.html
 
+import time
+
 Import("env")
 
 #
@@ -18,16 +20,20 @@ env.Append(
   ]
 )
 
-# Cube IDE link command
-# arm-none-eabi-g++ -o "cube_ide.elf" @"objects.list"   
-# -mcpu=cortex-m7 
-# -T"C:\Users\user\projects\daq\repo\controller\cube_ide\STM32H750VBTX_FLASH.ld" 
-# --specs=nosys.specs 
-# -Wl,-Map="cube_ide.map" 
-# -Wl,--gc-sections 
-# -static 
-# --specs=nano.specs 
-# -mfpu=fpv5-d16 
-# -mfloat-abi=hard 
-# -mthumb -Wl,
-# --start-group -lc -lm -lstdc++ -lsupc++ -Wl,--end-group
+# References:
+#   https://community.platformio.org/t/specifying-test-port-breaks-the-test/35344
+#   https://docs.platformio.org/en/latest/scripting/actions.html
+def after_upload(source, target, env):
+    # NOTE: Run 'pio test -vv' to make the print() messages visible in testing.
+    print(f"(extra_scripts.py): after_reload() called.")
+    build_type = env.GetProjectOption("build_type")
+    print(f"(extra_scripts.py): build_type = [{build_type}].")
+    if build_type == "debug":
+      print(f"(extra_scripts.py): Build type is debug, delaying...")
+      time.sleep(1)
+    else:
+      print(f"(extra_scripts.py): Build type is not debug.")
+
+    
+env.AddPostAction("upload", after_upload)
+
