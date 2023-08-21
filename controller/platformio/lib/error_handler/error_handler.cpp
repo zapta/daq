216316eb7b4a63fc8b46a0937157860356e124de
Zapta <zapta@users.noreply.github.com>
2023-08-21 12:59:54 -0700

    Added LED signaling in the error handler.

--- controller/platformio/lib/error_handler/error_handler.cpp
+++ controller/platformio/lib/error_handler/error_handler.cpp
@@ -1,19 +1,80 @@
 #include "error_handler.h"
 
+#include "gpio_pins.h"
 #include "main.h"
 
 #pragma GCC push_options
 #pragma GCC optimize("Og")
 
-void App_Error_Handler() { Error_Handler(); }
+static constexpr uint64_t kDelayUint = 25000000;
 
-// This is also called by HAL and the cube_ide libraries.
-void Error_Handler() {
-  __disable_irq();
-  // TODO: Blink LEDs or something.
-  while (1) {
+// Busy loop delay, in units of one beat.
+static void delay(float t) {
+  const uint64_t l = kDelayUint * t;
+  for (uint64_t i = 0; i < l; i++) {
     asm("nop");
   }
+}
+
+// Signal one digit
+static void send_digit(uint8_t n) {
+  if (n < 1) {
+    n = 1;
+  }
+  if (n > 9) {
+    n = 9;
+  }
+  delay(7);  // pre digit space
+  for (int i = 0; i < n; i++) {
+    gpio_pins::LED.set_high();
+    delay(0.4);
+    gpio_pins::LED.set_low();
+    delay(1.6);
+  }
+}
+
+ void App_Error_Handler(uint32_t e) {
+  __disable_irq();
+
+  for (;;) {
+    // Limit to three digits
+    if (e > 999) {
+      e = 999;
+    }
+
+    uint32_t d1 = e / 100;
+    uint32_t d2 = (e % 100) / 10;
+    uint32_t d3 = e % 10;
+
+    // Start marker (long pulse)
+    gpio_pins::LED.set_low();
+    delay(8.0);  // Pre sycle space
+    gpio_pins::LED.set_high();
+    delay(10.0);
+    gpio_pins::LED.set_low();
+
+    // Emit the digits
+    if (d1 > 0) {
+      send_digit(d1);
+    }
+    if (d1 > 0 || d2 > 0) {
+      send_digit(d2);
+    }
+    send_digit(d3);
+  }
+}
+
+
+// Error_Handler() is declared by the clube_ide file main.h.
+// This is also called by HAL and the cube_ide libraries.
+void Error_Handler() {
+  App_Error_Handler(3);
+
+  // __disable_irq();
+  // // TODO: Blink LEDs or something.
+  // while (1) {
+  //   asm("nop");
+  // }
 }
 
 #pragma GCC pop_options
--- controller/platformio/lib/error_handler/error_handler.h
+++ controller/platformio/lib/error_handler/error_handler.h
@@ -2,7 +2,14 @@
 
 #include "main.h"
 
-// Error_Handler() is declared by the clube_ide file main.h.
 
-// Used by the app.
-void App_Error_Handler();
+// Trap a fatal error of given code. e should be in [1, 999]
+// with zeros allows only as leading zeros, otherwise force
+// to be 1. E.g. 208 is displayed as 218 since we cannot signal
+// the digit 0.
+// App code should call this function rather than then Error_Handler
+// which is used by HAL and cube_ide libraries.
+void App_Error_Handler(uint32_t e = 5);
+
+// Temporary for developement.
+// void signal_error_code(uint32_t e);
