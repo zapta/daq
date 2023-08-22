4f2db6bebbeb41f00bdeab09bf413e86fce931a3
Zapta <zapta@users.noreply.github.com>
2023-08-22 14:11:33 -0700

    Now performing per channel global interpolation to have consistent 1ms data points.

--- host/lib/data_utils.py
+++ host/lib/data_utils.py
@@ -16,18 +16,6 @@ from dataclasses import dataclass
 
 logger = logging.getLogger("main")
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--input_dir",
-#                     dest="input_dir",
-#                     default="",
-#                     help="Input directory with channels .csv files.")
-# parser.add_argument("--output_dir",
-#                     dest="output_dir",
-#                     default="",
-#                     help="Output directory for generated files.")
-
-# args = parser.parse_args()
-
 
 @dataclass(frozen=True)
 class TestInfo:
@@ -35,9 +23,6 @@ class TestInfo:
     test_name: str
     start_ms: int
     end_ms: int
-
-
-
 
 
 def load_tests_infos(tests_file_path: str) -> List[TestInfo]:
@@ -74,26 +59,5 @@ def load_channels_infos(channels_file_path: str) -> List[ChannelInfo]:
         value_field = row["Field"]
         num_values = row["Values"]
         file_name = row["File"]
-        result.append(ChannelInfo(channel_name, channel_type, value_field, num_values,  file_name))
+        result.append(ChannelInfo(channel_name, channel_type, value_field, num_values, file_name))
     return result
-
-
-def extract_test_data(channel_df: pd.DataFrame, test_info: TestInfo, original_value_column: str,
-                      new_value_column: str):
-    """Extracts data of a single test."""
-    logger.info(f"Extracting: {test_info}")
-    # Load original
-    # df = pd.read_csv(data_file_path, delimiter=',')
-    # Select columns of interest
-    df = channel_df[['T[ms]', original_value_column]]
-    # Extract rows in the test time range
-    df = df[df['T[ms]'].between(test_info.start_ms, test_info.end_ms)]
-    # Normalize time to test start time.
-    df['T[ms]'] -= test_info.start_ms
-    # Rename the value column
-    if original_value_column != new_value_column:
-      df.rename(columns={original_value_column: new_value_column}, inplace=True)
-    # All done
-    return df
-
-
