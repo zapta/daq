af263c3fce94c2318a78e42bfd19e93f9d7e6e6e
Zapta <zapta@users.noreply.github.com>
2023-08-22 19:33:54 -0700

    Cleaned up the test collector.

--- host/src/tests_collector.py
+++ host/src/tests_collector.py
@@ -47,18 +47,21 @@ def main():
 
     tests_data = []
     for channel_info in channels_infos:
+        logger.info(f"Loading channel {channel_info.channel_name}[{channel_info.file_name}]")
         # Load the channel data
         channel_df = pd.read_csv(os.path.join(args.input_dir, channel_info.file_name), delimiter=',')
         # Select the timestamp and main value columns
         channel_df = channel_df[['T[ms]', channel_info.field_name]]
 
         # Add rows for missing ms timestamps. Missing values becomes None
+        logger.info("Up sampling.")
         ms_min = round(channel_df['T[ms]'].min())
         ms_max = round(channel_df['T[ms]'].max())
         ms_range = range(ms_min, (ms_max + 1))
         channel_df = channel_df.complete({"T[ms]": ms_range}, sort=True)
 
         # Interpolate to fill in the missing values.
+        logger.info("Interpolating.")
         channel_df.set_index('T[ms]', inplace=True)
         channel_df.interpolate(method='polynomial', order=2, inplace=True)
         channel_df.reset_index(inplace=True)
@@ -67,7 +70,8 @@ def main():
         # and missing values are interpolated.
 
         for test_info in test_infos:
-            # print(f"{test_info.test_name}-{channel_info.channel_name}")
+            logger.info(f"Selectign test {test_info.test_name}")
+            print(f"{test_info.test_name}-{channel_info.channel_name}")
             new_column_name = f"{channel_info.channel_name}/{test_info.test_name}"
 
             # df = extract_test_data(channel_df, test_info, channel_info.field_name, new_column_name)
@@ -80,6 +84,7 @@ def main():
             tests_data.append(df)
 
     # Join the tests data to a single data frame with common time column.
+    logger.info("Joining tests.")
     merged_df = None
     for df in tests_data:
         if merged_df is None:
@@ -97,11 +102,17 @@ def main():
 
     # Write out the file
     csv_output_file = os.path.join(args.output_dir, "_tests_data.csv")
-    logger.info(f"Writing results to file [{csv_output_file}]")
+    logger.info(f"Writing to [{csv_output_file}]")
     merged_df.to_csv(csv_output_file, index=False, float_format="%.3f", header=True)
 
     # Plot for sanity check
-    merged_df.plot(x="T[s]", xlabel="Seconds")
+
+    # Create a subsample for display. We pick only the 50th row.
+    # TODO: Select automatically the optimal subsampling factor
+    logger.info("Down sampling data.")
+    sub_sample = merged_df.iloc[::50, :]
+    logger.info("Plotting data.")
+    sub_sample.plot(x="T[s]", xlabel="Seconds")
     plt.show()
 
     logger.info(f"All done.")
