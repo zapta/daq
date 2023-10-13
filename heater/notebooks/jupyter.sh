baf7222287c793c342cce8d1c52e69c3a06dfc0b
Zapta <zapta@users.noreply.github.com>
2023-10-13 14:08:57 -0700

    More progress on the heater design.

---
+++ heater/notebooks/jupyter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# A shell script to run Jupyter Notebook. This runs a local server and opens
+# a GUI in a web browser.
+#
+# Precondition:
+#   pip install jupyter
+#
+# After starting this program, the dashboard is available at 
+#   http://localhost:8888/tree
+
+jupyter notebook
+
