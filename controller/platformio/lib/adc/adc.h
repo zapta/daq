f07ed5b4b486846a237b91fb82ccbbd1ab3feb26
Zapta <zapta@users.noreply.github.com>
2023-11-04 11:36:50 -0700

    Updated ADC header comments. No semantic change.

--- controller/platformio/lib/adc/adc.h
+++ controller/platformio/lib/adc/adc.h
@@ -1,21 +1,18 @@
-#pragma once
+// Handler of the load cell and temperature ADC board (ADS1261/SPI).
+// Not to be confused with the current/voltage I2C ADC card.
 
-// class Adc {
-//  public:
-//  private:
-// };
+// TODO: Find a better name for this module.
+
+#pragma once
 
 namespace adc {
 
-// void test_setup();
-// void test_loop();
+// Does not return.
+void adc_task_body(void* argument);
 
-  // Does not return.
-  void adc_task_body(void* argument);
+void dump_state();
 
-  void dump_state();
-
-  // For diagnostics.
-  void verify_registers_vals();
+// For diagnostics.
+void verify_registers_vals();
 
 }  // namespace adc
